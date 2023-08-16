01d2c6c3aedabcc497b83ef0a2381c1bdc4b0447
Anton Patrushev <apatrushev@gmail.com>
2023-08-17 04:25:48 +0500

    switch to pyproject.toml

--- tasks.py
+++ tasks.py
@@ -1,1 +1,3 @@
-from spherical.dev.tasks import clean, flake, isort, test  # noqa: F401
+from spherical.dev.tasks import test  # noqa: F401
+from spherical.dev.tasks import clean, flake, isort  # noqa: F401
+from spherical.dev.tasks import pypi_release as release  # noqa: F401
